da7db967d769412ec17493323383ad0ed329c2f5
Nicole <nicole.2021.nh@gmail.com>
2021-07-21 12:38:11 -0500

    fix

--- 8alance Project/ViewController.swift
+++ 8alance Project/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var shakeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func becomeFirstResponder() -> Bool {
+        return true
+        
+    }
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            shakeLabel.text = "Shake me"
+    }
 }
 
+}
